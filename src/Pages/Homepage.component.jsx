8ef480c48d39e70ee476e16b85fd0b48826635e9
Elsayed4 <39974553+Elsayed4@users.noreply.github.com>
2021-02-07 20:57:13 +0200

    "Frist E-Commerce App"

---
+++ src/Pages/Homepage.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Homepage.style.scss';
+import Directory from '../Component/Directory.component'
+const Homepage = () =>{
+  return(
+      <div className="homepage">
+          <Directory />
+      </div>
+  )
+}
+
+export default Homepage;
