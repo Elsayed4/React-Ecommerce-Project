b133653852fe01a45e60187c9350df8a610b0a23
Elsayed4 <39974553+Elsayed4@users.noreply.github.com>
2021-02-07 21:00:52 +0200

    dd

--- src/Component/Menuitem.component.jsx
+++ src/Component/Menuitem.component.jsx
@@ -9,7 +9,7 @@ const Menuitem = ({title,imageUrl,size}) =>{
     }}>
          <div className="content">
             <h1 className="title">{title.toUpperCase()}</h1>
-            <span className="subtitle">SHOP NOW</span>
+            <span className="subtitle"> SHOP NOWW </span>
          </div>
        </div>
  );
